9a132b3346110f8eea78c027d0ccc09b9963fc00
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-11-16 12:13:51 +0000

    add base template to templates included bootstrap cdn and custom css links

--- allicatorComplaints/views.py
+++ allicatorComplaints/views.py
@@ -1,3 +1,11 @@
+"""
+1 - Importing render shortcut.
+2 - Importing generic view template from django views.
+3 - 
+"""
 from django.shortcuts import render
+from django.views import generic
+from .models import Complaint
 
 # Create your views here.
+# class ComplaintList(generic.ListView):
