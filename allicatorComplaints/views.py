9c9963f854202bae3688f0012082f75414b3e864
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-11-18 16:19:43 +0000

    updated edit_complaint.html with correct url syntax in form, updated edit _complaint view to accomodate update db record, CRU functionality working

--- allicatorComplaints/views.py
+++ allicatorComplaints/views.py
@@ -42,32 +42,23 @@ def add_complaint(request):
             latest_update=latest_update,
         )
         return redirect('home')
-    return render(request, 'add_complaint.html', context)
+    return render(request, 'add_complaint.html')
 
 
 def edit_complaint(request, log_number):
     complaint = get_object_or_404(Complaint, log_number=log_number)
     if request.method == 'POST':
-        log_number = request.POST.get('log_number')
-        customer_surname = request.POST.get('customer_surname')
-        complaint_category = request.POST.get('complaint_category')
-        date_logged = request.POST.get('date_logged')
-        case_owner = request.user
-        welcome_email = 'welcome_email' in request.POST
-        customer_contacted = 'customer_contacted' in request.POST
-        holding_correspondence = 'holding_correspondence' in request.POST
-        outstanding_actions = 'outstanding_actions' in request.POST
-        latest_update = request.POST.get('latest_update')
-        Complaint.save(
-            customer_surname=customer_surname,
-            complaint_category=complaint_category,
-            date_logged=date_logged,
-            welcome_email=welcome_email,
-            customer_contacted=customer_contacted,
-            holding_correspondence=holding_correspondence,
-            outstanding_actions=outstanding_actions,
-            latest_update=latest_update,
-        )
+        complaint.log_number = request.POST.get('log_number')
+        complaint.customer_surname = request.POST.get('customer_surname')
+        complaint.complaint_category = request.POST.get('complaint_category')
+        complaint.date_logged = request.POST.get('date_logged')
+        complaint.case_owner = request.user
+        complaint.welcome_email = 'welcome_email' in request.POST
+        complaint.customer_contacted = 'customer_contacted' in request.POST
+        complaint.holding_correspondence = 'holding_correspondence' in request.POST
+        complaint.outstanding_actions = 'outstanding_actions' in request.POST
+        complaint.latest_update = request.POST.get('latest_update')
+        complaint.save(update_fields=None)
         return redirect('home')
     context = {
         'complaint': complaint,
