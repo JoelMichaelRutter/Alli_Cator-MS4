bf984026fedb9edb1f2ff3a73d5d43fe73bfdeb3
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-11-27 16:51:12 +0000

    fix: linter issues in allicatorComplaints app urls.py file

--- allicatorComplaints/urls.py
+++ allicatorComplaints/urls.py
@@ -1,7 +1,6 @@
 """
-1 -
-2 -
-3 -
+1 - Importing path method for use in URL patterns list
+2 - Importing all views for use in URL patterns list
 """
 from django.urls import path
 from . import views
@@ -10,6 +9,12 @@ from . import views
 urlpatterns = [
     path('', views.ViewComplaintList.as_view(), name="home"),
     path('add-complaint', views.add_complaint, name='add-complaint'),
-    path('edit-complaint/<log_number>/', views.edit_complaint, name='edit-complaint'),
-    path('delete-complaint/<log_number>/', views.delete_complaint, name='delete-complaint')
+    path('edit-complaint/<log_number>/',
+         views.edit_complaint,
+         name='edit-complaint'
+         ),
+    path('delete-complaint/<log_number>/',
+         views.delete_complaint,
+         name='delete-complaint'
+         )
 ]
