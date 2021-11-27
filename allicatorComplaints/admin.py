788619f0d6c5c1ffab52c3cf97f5bba92fb55b8b
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-11-27 16:35:39 +0000

    add additional detail to docstring at top of admin.py file

--- allicatorComplaints/admin.py
+++ allicatorComplaints/admin.py
@@ -1,15 +1,13 @@
 """
 IMPORTS
 1 - Imorting admin library
-2 - Importing installed django summernote model admin
-3 - Importing complaint data model
+2 - Importing installed django summernote model admin to use
+filtering, display and search functionality in Admin site.
+3 - Importing Complaint data model
 """
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Complaint
-
-
-# Register your models here.
 
 
 @admin.register(Complaint)
